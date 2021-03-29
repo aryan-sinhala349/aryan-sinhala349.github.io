9dcdbb127447db3fd19a28c4031208e53e5a4505
Aryan Sinha <aryananusinha@gmail.com>
2021-03-29 18:43:21 -0400

    Fixed width detection method to make the navbar work better on mobile devices

--- scripts/nav.js
+++ scripts/nav.js
@@ -1,9 +1,11 @@
 function openNav() {
-    let viewportWidth = window.innerWidth;
+    let viewportWidth = window.screen.availWidth;
 
     if (viewportWidth < 480)
         document.getElementsByTagName("nav")[0].style.width = "100%";
     else if (viewportWidth < 800)
+        document.getElementsByTagName("nav")[0].style.width = "75%";
+    else if (viewportWidth < 1200)
         document.getElementsByTagName("nav")[0].style.width = "50%";
     else
         document.getElementsByTagName("nav")[0].style.width = "25%";
