6b83b468c6f384cc35485d3dc6479d8aa2746d2f
Aryan Sinha <aryananusinha@gmail.com>
2021-03-29 19:02:48 -0400

    Directly checking if the device has the Mobi tag to determine if the device is a mobile device

--- scripts/nav.js
+++ scripts/nav.js
@@ -1,12 +1,6 @@
 function openNav() {
-    let viewportWidth = (window.innerWidth > 0) ? window.innerWidth : screen.width;
-
-    if (viewportWidth < 480)
+    if (navigator.userAgent.indexOf("Mobi") > -1)
         document.getElementsByTagName("nav")[0].style.width = "100%";
-    else if (viewportWidth < 800)
-        document.getElementsByTagName("nav")[0].style.width = "75%";
-    else if (viewportWidth < 1200)
-        document.getElementsByTagName("nav")[0].style.width = "50%";
     else
         document.getElementsByTagName("nav")[0].style.width = "25%";
 }
