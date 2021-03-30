b9e796f55d817f80e816f85b4be1781e223b213b
Aryan Sinha <aryananusinha@gmail.com>
2021-03-30 17:41:02 -0400

    Changed navigation bar size when window size is adjusted

--- scripts/nav.js
+++ scripts/nav.js
@@ -1,3 +1,9 @@
+let open = false;
+
+window.addEventListener('resize', () => {
+    if (open) openNav();
+});
+
 function openNav() {
     //Get the aspect ratio of the viewport
     const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
@@ -8,11 +14,17 @@ function openNav() {
     //If the aspect ratio is less than 1 the device is in portrait mode, so fill the entire screen
     if (aspectRatio < 1)
         document.getElementsByTagName("nav")[0].style.width = "100%";
-    //Otherwise the viewport is either perfectly square or in landscape mode, so only fill 25% of the screen
+    //Otherwise the viewport is either perfectly square or in landscape mode, so only fill 30% of the screen
     else
-        document.getElementsByTagName("nav")[0].style.width = "25%";
+        document.getElementsByTagName("nav")[0].style.width = "30%";
+
+    open = true;
 }
+
+
 
 function closeNav() {
     document.getElementsByTagName("nav")[0].style.width = "0%";
+
+    open = false;
 }
